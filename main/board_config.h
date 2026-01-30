8f8efe163b086ce68f88d9ae1648677a1603ccee
Shaun Foulkes <ShaunPCcom@users.noreply.github.com>
2026-01-30 18:15:21 -0400

    esp32-h2: switch target and set LD2450 UART pins to GPIO8/9

--- main/board_config.h
+++ main/board_config.h
@@ -3,10 +3,10 @@
 #include "driver/uart.h"
 
 // WROOM32 + LD2450 wiring (matches your working ESPHome YAML):
-// uart rx_pin: GPIO16  (ESP32 RX)  <- LD2450 TX
-// uart tx_pin: GPIO17  (ESP32 TX)  -> LD2450 RX
+// uart rx_pin: GPIO8  (ESP32 RX)  <- LD2450 TX
+// uart tx_pin: GPIO9  (ESP32 TX)  -> LD2450 RX
 #define LD2450_UART_NUM      UART_NUM_1
-#define LD2450_UART_TX_GPIO  GPIO_NUM_17
-#define LD2450_UART_RX_GPIO  GPIO_NUM_16
+#define LD2450_UART_TX_GPIO  GPIO_NUM_8
+#define LD2450_UART_RX_GPIO  GPIO_NUM_9
 #define LD2450_UART_BAUD     256000
 
